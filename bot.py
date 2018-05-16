6092efb203099c266b2865a52fae6727646cd35f
Savagecoder77 <savagecoder77@gmail.com>
2018-05-15 17:02:45 -0700

    added begginings of bot

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
+#not named bot yet
 
+import discord
+from discord.ext import commands
+from discord.ext.commands import Bot
+import asyncio
+import chalk
+
+bot = commands.Bot(command_prefix='.')
+
+@bot.event
+async def on_ready():
+    print ("Ready when you are")
+    print ("I am running on " + bot.user.name)
+    print ("With the ID: " + bot.user.id)
+
+bot.run('TOKEN')
